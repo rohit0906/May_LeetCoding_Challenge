8ae19318d6b256e4f7dce950efba744f0d6e5787
Rohit Verma <63773928+rohit0906@users.noreply.github.com>
2020-05-08 13:36:31 +0530

    Create   Check If It Is a Straight Line.py

---
+++ Check If It Is a Straight Line.py
@@ -0,0 +1,54 @@
+class Solution:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        if len(coordinates)<=2:
+            return True
+        try:
+            m=(coordinates[1][1]-coordinates[0][1])/(coordinates[1][0]-coordinates[0][0])
+        except:
+            return False
+        for x,y in coordinates[2:]:
+            if (y-coordinates[1][1])!=m*(x-coordinates[1][0]):
+                return False
+            
+        return True
+        
+        
+        
+ '''
+ -------------------------------------------***PORBLEM STATEMENT***------------------------------------------------
+ 
+ You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
+
+ 
+
+ 
+
+Example 1:
+
+
+
+Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
+Output: true
+Example 2:
+
+
+
+Input: coordinates = [[1,1],[2,2],[3,4],[4,5],[5,6],[7,7]]
+Output: false
+ 
+
+Constraints:
+
+2 <= coordinates.length <= 1000
+coordinates[i].length == 2
+-10^4 <= coordinates[i][0], coordinates[i][1] <= 10^4
+coordinates contains no duplicate point.
+ 
+ '''
+ 
+ '''
+66 / 66 test cases passed.
+Status: Accepted
+Runtime: 60 ms
+Memory Usage: 14.2 MB
+ '''
